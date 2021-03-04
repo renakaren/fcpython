b56c537c380722f803037eb30cac365b401d87f7
renakaren <68769205+renakaren@users.noreply.github.com>
2021-03-04 21:29:48 +0800

    Add files via upload

---
+++ 迴圈實例.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  4 21:05:25 2021
+
+@author: user
+"""
+
+
+'''
+#for 自己取的變數名稱
+for i in range(10):
+    print(i)
+print("程式執行完畢")#range決定程式執行幾次
+'''
+
+
+for i in range(1,10):
+    if i % 2 == 0:
+        print(i, "是偶數")#字串須加上"" or ''
+    
+print("程式執行完畢")
